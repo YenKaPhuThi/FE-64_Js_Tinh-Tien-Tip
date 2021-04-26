d710f03bc57ea34044994f5fa3c5fd42306f1ff1
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-04-26 07:55:37 +0700

    [Js]: Handle logic Tinh Tien Tip and ui

--- js/main.js
+++ js/main.js
@@ -1,16 +1,25 @@
 // Define Global Variables
 var paymentAmount = document.getElementById("paymentAmount");
+var numberSharing = document.getElementById("numberSharing");
 var tippingOptions = document.getElementById("tippingOptions");
-var btnPaymentCaculate = document.getElementById("btnPaymentCaculate");
-
-var paymentAmountVal = paymentAmount.value;
-var optionSelected = tippingOptions.options[tippingOptions.selectedIndex].value;
+var tippingNoticed = document.getElementById("tippingNoticed");
+var btnPaymentaculate = document.getElementById("btnPaymentCaculate");
 
 // Handle Payment Caculation
-function handlePaymentCaculation(amount, number) {
-  btnPaymentCaculate.addEventListener("click", function() {
+function handlePaymentCaculation() {
+  btnPaymentCaculate.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    var paymentAmountVal = paymentAmount.value;
+    var numberSharingVal = numberSharing.value;
+    var optionSelected = tippingOptions.options[tippingOptions.selectedIndex].value;
     var paymentCaculation = 0;
+
+    paymentCaculation = (paymentAmountVal * optionSelected) / 100 / numberSharingVal;
+    
+    tippingNoticed.style.display = 'block';
+    tippingNoticed.innerHTML = "<p>" + paymentCaculation + " Mỗi người</p>";
   });
 }
 
-handlePaymentCaculation(200, 3);
+handlePaymentCaculation();
