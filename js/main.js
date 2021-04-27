f5e119ae111a41057d487feee3c769696cebca5c
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-04-27 22:18:19 +0700

    [Js]: Fix issue in case user doesn't input value to Number Sharing field

--- js/main.js
+++ js/main.js
@@ -1,16 +1,14 @@
-// Define Global Variables
-var infoNotice     = document.getElementById("infoNotice");
-var btnPayment     = document.getElementById("btnPayment");
-var paymentAmount  = document.getElementById("paymentAmount");
-var numberSharing  = document.getElementById("numberSharing");
-var tippingOptions = document.getElementById("tippingOptions");
-
 // Handle Payment Caculation
 function handlePaymentCaculation() {
-  btnPayment.addEventListener("click", function() {
+  document.getElementById("btnPayment").addEventListener("click", function() {
+    var infoNotice     = document.getElementById("infoNotice");
+    var paymentAmount  = document.getElementById("paymentAmount");
+    var numberSharing  = document.getElementById("numberSharing");
+    var tippingOptions = document.getElementById("tippingOptions");
+
     var paymentAmountVal  = paymentAmount.value;
     var numberSharingVal  = numberSharing.value;
-    var tippingOptionsVal = tippingOptions.options[tippingOptions.selectedIndex].value;
+    var tippingOptionsVal = tippingOptions.options[tippingOptions?.selectedIndex].value;
     var paymentCaculation = 0;
 
     // Check Payment Amount is empty => don't do anything
@@ -23,10 +21,9 @@ function handlePaymentCaculation() {
     if (isNaN(paymentAmountVal) || isNaN(numberSharingVal)) {
       infoNotice.innerHTML = '<p class="text-notice">' + "Vui lòng nhập số!" + "</p>";
     } else {
-      numberSharing  = 0;
-      paymentAmount  = parseFloat(paymentAmountVal);
-      tippingOptions = parseFloat(tippingOptionsVal);
-      numberSharing  = !numberSharingVal.length ? 0 : parseFloat(numberSharingVal);
+      paymentAmount  = parseInt(paymentAmountVal);
+      tippingOptions = parseInt(tippingOptionsVal);
+      numberSharing  = numberSharingVal.length ? parseInt(numberSharingVal) : 1;
 
       paymentCaculation = ((paymentAmount * tippingOptions) / 100) / numberSharing;
       infoNotice.innerHTML = "<p><sup>$</sup>" + '<span class="fs-20">' + paymentCaculation + "</span>" + "<br/>mỗi người</p>";
