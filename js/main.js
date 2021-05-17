c507309f761d65a754d8b6ce189bd035972c2d66
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-17 21:43:04 +0700

    [Js]: Handle valiation that check empty input's value and index select

--- js/main.js
+++ js/main.js
@@ -3,19 +3,42 @@ function getMyEleId(ele) {
   return document.getElementById(ele);
 }
 
+// Define error empty messages
+var errorEmptyMsg = [
+  "Tổng tiền không được bỏ trống!",
+  "Chọn để tip không được bỏ trống!",
+  "Nhập số người không được bỏ trống!",
+];
+
+// Handle check input's value is empty and select index is 0
+function checkEmptyValue(fieldEle, errorEle, indexMsg) {
+  var fieldEle = getMyEleId(fieldEle);
+  var errorEle = getMyEleId(errorEle);
+
+  errorEle.style.display = "none";
+  if (fieldEle.value === "" || fieldEle.selectedIndex == 0) {
+    errorEle.style.display = "block";
+    errorEle.textContent = errorEmptyMsg[indexMsg];
+  }
+}
+
 // Handle Payment Caculation
 function handlePayment() {
   getMyEleId("btnPayment").addEventListener("click", function () {
     var infoNotice = getMyEleId("infoNotice");
     var paymentAmount = getMyEleId("paymentAmount");
     var numberSharing = getMyEleId("numberSharing");
-    var tippingOptions = getMyEleId("tippingOptions");
+    var tipOptions = getMyEleId("tipOptions");
 
     var paymentAmountVal = paymentAmount.value;
     var numberSharingVal = numberSharing.value;
-    var tippingOptionsVal =
-      tippingOptions.options[tippingOptions?.selectedIndex].value;
+    var tipOptionsVal = tipOptions.options[tipOptions?.selectedIndex].value;
     var paymentCaculation = 0;
+
+    // Check input's value is empty => show error message
+    checkEmptyValue("paymentAmount", "errorAmount", 0);
+    checkEmptyValue("tipOptions", "errorTip", 1);
+    checkEmptyValue("numberSharing", "errorSharing", 2);
 
     // Check Payment Amount is empty => don't do anything
     // - This field is required
@@ -29,11 +52,10 @@ function handlePayment() {
         '<p class="text-notice">' + "Vui lòng nhập số!" + "</p>";
     } else {
       paymentAmount = parseInt(paymentAmountVal);
-      tippingOptions = parseInt(tippingOptionsVal);
+      tipOptions = parseInt(tipOptionsVal);
       numberSharing = numberSharingVal.length ? parseInt(numberSharingVal) : 1;
 
-      paymentCaculation =
-        (paymentAmount * tippingOptions) / 100 / numberSharing;
+      paymentCaculation = (paymentAmount * tipOptions) / 100 / numberSharing;
       infoNotice.innerHTML =
         "<p><sup>$</sup>" +
         '<span class="fs-20">' +
