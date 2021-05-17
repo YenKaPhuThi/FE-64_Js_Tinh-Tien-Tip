9d77093225219fd006cb3e736def1e7319444694
Yen Chipu <yenkaphu96pnv@gmail.com>
2021-05-17 21:03:07 +0700

    [Js]: Optimize code and format code to easily read

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
-// Handle Payment Caculation
-function handlePaymentCaculation() {
-  document.getElementById("btnPayment").addEventListener("click", function() {
-    var infoNotice     = document.getElementById("infoNotice");
-    var paymentAmount  = document.getElementById("paymentAmount");
-    var numberSharing  = document.getElementById("numberSharing");
-    var tippingOptions = document.getElementById("tippingOptions");
+// Get element by Id
+function getMyEleId(ele) {
+  return document.getElementById(ele);
+}
 
-    var paymentAmountVal  = paymentAmount.value;
-    var numberSharingVal  = numberSharing.value;
-    var tippingOptionsVal = tippingOptions.options[tippingOptions?.selectedIndex].value;
+// Handle Payment Caculation
+function handlePayment() {
+  getMyEleId("btnPayment").addEventListener("click", function () {
+    var infoNotice = getMyEleId("infoNotice");
+    var paymentAmount = getMyEleId("paymentAmount");
+    var numberSharing = getMyEleId("numberSharing");
+    var tippingOptions = getMyEleId("tippingOptions");
+
+    var paymentAmountVal = paymentAmount.value;
+    var numberSharingVal = numberSharing.value;
+    var tippingOptionsVal =
+      tippingOptions.options[tippingOptions?.selectedIndex].value;
     var paymentCaculation = 0;
 
     // Check Payment Amount is empty => don't do anything
@@ -19,16 +25,23 @@ function handlePaymentCaculation() {
 
     // Check input's value is number
     if (isNaN(paymentAmountVal) || isNaN(numberSharingVal)) {
-      infoNotice.innerHTML = '<p class="text-notice">' + "Vui lòng nhập số!" + "</p>";
+      infoNotice.innerHTML =
+        '<p class="text-notice">' + "Vui lòng nhập số!" + "</p>";
     } else {
-      paymentAmount  = parseInt(paymentAmountVal);
+      paymentAmount = parseInt(paymentAmountVal);
       tippingOptions = parseInt(tippingOptionsVal);
-      numberSharing  = numberSharingVal.length ? parseInt(numberSharingVal) : 1;
+      numberSharing = numberSharingVal.length ? parseInt(numberSharingVal) : 1;
 
-      paymentCaculation = ((paymentAmount * tippingOptions) / 100) / numberSharing;
-      infoNotice.innerHTML = "<p><sup>$</sup>" + '<span class="fs-20">' + paymentCaculation + "</span>" + "<br/>mỗi người</p>";
+      paymentCaculation =
+        (paymentAmount * tippingOptions) / 100 / numberSharing;
+      infoNotice.innerHTML =
+        "<p><sup>$</sup>" +
+        '<span class="fs-20">' +
+        paymentCaculation +
+        "</span>" +
+        "<br/>mỗi người</p>";
     }
   });
 }
 
-handlePaymentCaculation();
+handlePayment();
